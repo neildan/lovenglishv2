0af7b41a4829fc9c6c4c4587f822d2c776139d19
Daniel Felipe Luis Valencia <felipeluiz152000@gmail.com>
2020-04-12 13:15:57 -0500

    Se cambia nombre en el manifest y se modifica el sw

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+document.cookie = 'same-site-cookie=foo; SameSite=Lax';
+document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
+
 const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
-
-/* Put code here */
 
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log('👍', 'beforeinstallprompt', event);
@@ -35,10 +36,42 @@ window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
 });
 
-/* Only register a service worker if it's supported */
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
+
+    navigator.serviceWorker
+        .register('./sw.js', { scope: './' })
+        .then(function (registration) {
+            console.log("Service Worker Registered");
+        })
+        .catch(function (err) {
+            console.log("Service Worker Failed to Register", err);
+        })
+
 }
+
+// Function to perform HTTP request
+var get = function (url) {
+    return new Promise(function (resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var result = xhr.responseText
+                    result = JSON.parse(result);
+                    resolve(result);
+                } else {
+                    reject(xhr);
+                }
+            }
+        };
+
+        xhr.open("GET", url, true);
+        xhr.send();
+
+    });
+};
 
 /**
  * Warn the page must be served over HTTPS
--- sw.js
+++ sw.js
@@ -1,80 +1,153 @@
-;
-//asignar un nombre y versión al cache
-const cacheName = 'love_english_cache',
-  urlsToCache = [
-    './index.html',
-    './script.js',
-    './css/bootstrap.min.css',
-    './css/jquery.mCustomScrollbar.css',
-    './css/styles.css',
-    './fonts/Friendly-Schoolmates.otf',
-    './fonts/Mithella-Regular.otf',
-    './img/icons/brain-16.png',
-    './img/icons/brain-24.png',
-    './img/icons/brain-32.png',
-    './img/icons/brain-64.png',
-    './img/icons/brain-128.png',
-    './img/icons/brain-256.png',
-    './img/icons/brain-512.png',
-    './js/jquery.mCustomScrollbar.concat.min.js',
-    './pages/narnia.html',
-    './audios/1/main.mp3',
-    './audios/1/mini.mp3',
-    './audios/1/questions.mp3',
-    './audios/2/main.mp3',
-    './audios/2/mini.mp3',
-    './audios/2/questions.mp3',
-    './audios/3/main.mp3',
-    './audios/3/mini.mp3',
-    './audios/3/questions.mp3',
-    './audios/4/main.mp3',
-    './audios/4/mini.mp3',
-    './audios/4/questions.mp3',
-    './audios/5/main.mp3',
-    './audios/5/mini.mp3',
-    './audios/5/questions.mp3',
-    './audios/6/main.mp3',
-    './audios/6/mini.mp3',
-    './audios/6/questions.mp3',
-    './audios/7/main.mp3',
-    './audios/7/mini.mp3',
-    './audios/7/questions.mp3',
-    './audios/8/main.mp3',
-    './audios/8/mini.mp3',
-    './audios/8/questions.mp3',
-    './audios/9/main.mp3',
-    './audios/9/mini.mp3',
-    './audios/9/questions.mp3',
-    './audios/10/main.mp3',
-    './audios/10/mini.mp3',
-    './audios/10/questions.mp3',
-    './audios/11/main.mp3',
-    './audios/11/mini.mp3',
-    './audios/11/questions.mp3',
-    './audios/12/main.mp3',
-    './audios/12/mini.mp3',
-    './audios/12/questions.mp3'
-  ]
+// Set a name for the current cache
+var cacheName = 'v1_love_english_cache';
 
-self.addEventListener('install', e => {
+// Default files to always cache
+var cacheFiles = [
+  './',
+  './script.js',
+  './css/bootstrap.min.css',
+  './css/jquery.mCustomScrollbar.css',
+  './css/styles.css',
+  './fonts/Friendly-Schoolmates.otf',
+  './fonts/Mithella-Regular.otf',
+  './img/icons/brain-16.png',
+  './img/icons/brain-24.png',
+  './img/icons/brain-32.png',
+  './img/icons/brain-64.png',
+  './img/icons/brain-128.png',
+  './img/icons/brain-256.png',
+  './img/icons/brain-512.png',
+  './js/jquery.mCustomScrollbar.concat.min.js',
+  './pages/narnia.html',
+  './audios/1/main.mp3',
+  './audios/1/mini.mp3',
+  './audios/1/questions.mp3',
+  './audios/2/main.mp3',
+  './audios/2/mini.mp3',
+  './audios/2/questions.mp3',
+  './audios/3/main.mp3',
+  './audios/3/mini.mp3',
+  './audios/3/questions.mp3',
+  './audios/4/main.mp3',
+  './audios/4/mini.mp3',
+  './audios/4/questions.mp3',
+  './audios/5/main.mp3',
+  './audios/5/mini.mp3',
+  './audios/5/questions.mp3',
+  './audios/6/main.mp3',
+  './audios/6/mini.mp3',
+  './audios/6/questions.mp3',
+  './audios/7/main.mp3',
+  './audios/7/mini.mp3',
+  './audios/7/questions.mp3',
+  './audios/8/main.mp3',
+  './audios/8/mini.mp3',
+  './audios/8/questions.mp3',
+  './audios/9/main.mp3',
+  './audios/9/mini.mp3',
+  './audios/9/questions.mp3',
+  './audios/10/main.mp3',
+  './audios/10/mini.mp3',
+  './audios/10/questions.mp3',
+  './audios/11/main.mp3',
+  './audios/11/mini.mp3',
+  './audios/11/questions.mp3',
+  './audios/12/main.mp3',
+  './audios/12/mini.mp3',
+  './audios/12/questions.mp3'
+]
+
+
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Installed');
+
+  // e.waitUntil Delays the event until the Promise is resolved
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(urlsToCache)
-        .then(() => self.skipWaiting());
+
+    // Open the cache
+    caches.open(cacheName).then(function (cache) {
+
+      // Add all the default files to the cache
+      console.log('[ServiceWorker] Caching cacheFiles');
+      return cache.addAll(cacheFiles);
     })
-  );
+  ); // end e.waitUntil
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activated');
+
+  e.waitUntil(
+
+    // Get all the cache keys (cacheName)
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(cacheNames.map(function (thisCacheName) {
+
+        // If a cached item is saved under a previous cacheName
+        if (thisCacheName !== cacheName) {
+
+          // Delete that cached file
+          console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+          return caches.delete(thisCacheName);
+        }
+      }));
+    })
+  ); // end e.waitUntil
+
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+
+self.addEventListener('fetch', function (e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+
+  // e.respondWidth Responds to the fetch event
+  e.respondWith(
+
+    // Check in cache for the request being made
+    caches.match(e.request)
+
+
+      .then(function (response) {
+
+        // If the request is in the cache
+        if (response) {
+          console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+          // Return the cached version
+          return response;
+        }
+
+        // If the request is NOT in the cache, fetch and cache
+
+        var requestClone = e.request.clone();
+        return fetch(requestClone)
+          .then(function (response) {
+
+            if (!response) {
+              console.log("[ServiceWorker] No response from fetch ")
+              return response;
+            }
+
+            var responseClone = response.clone();
+
+            //  Open the cache
+            caches.open(cacheName).then(function (cache) {
+
+              // Put the fetched response in the cache
+              cache.put(e.request, responseClone);
+              console.log('[ServiceWorker] New Data Cached', e.request.url);
+
+              // Return the response
+              return response;
+
+            }); // end caches.open
+
+          })
+          .catch(function (err) {
+            console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+          });
+
+
+      }) // end caches.match(e.request)
+  ); // end e.respondWith
 });
